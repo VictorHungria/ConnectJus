51c6af2d0e30ed22b9340b757d41679ac240fb29
Victor H 2025 <victorhungria97@gmail.com>
2025-11-12 11:51:46 -0300

    Versão inicial da Landing Page 15

---
+++ components.tsx
@@ -0,0 +1,8 @@
+export { Header } from './components/Header';
+export { HeroSection } from './components/HeroSection';
+export { FeaturesSection } from './components/FeaturesSection';
+export { WorkflowSection } from './components/WorkflowSection';
+export { IntegratedToolsSection } from './components/IntegratedToolsSection';
+export { SecuritySection } from './components/SecuritySection';
+export { CtaSection } from './components/CtaSection';
+export { Footer } from './components/Footer';
