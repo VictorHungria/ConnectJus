f262e52f45787a584c5172920f9290a1d3bf698e
Victor H 2025 <victorhungria97@gmail.com>
2025-11-17 23:35:06 -0300

    Atualização do Link de Download

--- config.ts
+++ config.ts
@@ -4,4 +4,4 @@
  * You can edit the GITHUB_LATEST_RELEASE_URL to point to the direct download link for the latest release.
  */
 
-export const GITHUB_LATEST_RELEASE_URL = "https://github.com/VictorHungria/ConnectJus/releases/download/v0.0.4-alpha/ConnectJusDocs-Setup-0.0.4-alpha.exe";
+export const GITHUB_LATEST_RELEASE_URL = "https://github.com/VictorHungria/ConnectJus/releases/download/v0.0.5-alpha/ConnectJusDocs-Setup-0.0.5-alpha.exe";
